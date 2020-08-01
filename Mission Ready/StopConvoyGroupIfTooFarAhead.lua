0a00684323aca8debbc49c071461ba5a74d09844
Matthew Clairmont <matthew.clairmont1@gmail.com>
2020-08-01 17:15:48 -0400

    removed debug popups

--- Mission Ready/StopConvoyGroupIfTooFarAhead.lua
+++ Mission Ready/StopConvoyGroupIfTooFarAhead.lua
@@ -4,33 +4,28 @@
 -- returns float of 2D distance between 2 groups
 function GetGroupDistance(g1, g2)
 BASE:F3("Group Distance")
-env.warning(g1,false)
-env.warning(g2,false)
 grp1Coord = GROUP:FindByName(g1):GetCoordinate()
 grp2Coord = GROUP:FindByName(g2):GetCoordinate()
 dist = grp1Coord:Get2DDistance(grp2Coord)
-env.setErrorMessageBoxEnabled(true)
-env.warning(dist, true)
 return dist
 end
 
 
 function StopLeadIfTooFarAway(g1, g2, maxDist)
 BASE:E("StopLead")
-env.warning(g1,false)
-env.warning(g2,false)
 distBetweenGroups = GetGroupDistance(g1, g2)
 
 blah = math.floor(distBetweenGroups)
-env.warning(blah,true)
+env.info(blah, false)
  if blah >= maxDist then
 -- Get lead APC group units
 
 Sched = SCHEDULER:New(nil)
 ScheduleIDStop = Sched:ScheduleOnce(2,StopMove,g1,1,0)
 
-env.warning("Lead convoy too far; holding", false)
+env.info("Lead convoy too far; holding", false)
   else
+  env.info("Groups within range; starting convoy",false)
 ScheduleIDStart = Sched:ScheduleOnce(2,StartMove,g1,1,0)
   end
 end
@@ -54,5 +49,5 @@ end
  -- main starts here --
  -- CHANGE THESE FOR PROD --
 g1, g2 = "ConvoyLead", "ConvoySupplies"
-StopLeadIfTooFarAway(g1,g2,800) 
---sleepSched = SCHEDULER:New(nil, StopLeadIfTooFarAway, {g1, g2, maxDist}, 1, 15, 0, 0)
+--StopLeadIfTooFarAway(g1,g2,800) 
+sleepSched = SCHEDULER:New(nil, StopLeadIfTooFarAway, {g1, g2, 800}, 1, 15, 0, 0)
