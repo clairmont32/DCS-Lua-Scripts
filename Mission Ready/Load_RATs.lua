a94f4ea67142cda116208534107c34d107d20a1c
Matthew Clairmont <matthew.clairmont1@gmail.com>
2020-08-01 15:03:24 -0400

    remove redundant function

--- Mission Ready/Load_RATs.lua
+++ Mission Ready/Load_RATs.lua
@@ -1,16 +1,34 @@
-function spawnRAT(groupName, depart, FL, dist, amt) 
+-- change bottom start/dest and amount
+
+function spawnRAT(groupName, depart, FL, dist, amt) -- doesnt care where they go
 local rat=RAT:New(groupName)
 rat:SetDeparture(depart)
 rat:SetFLcruise(FL)
 rat:SetMaxDistance(dist)
-rat:SetTakeoff("cold")
+rat:SetTakeoff("runway")
 rat:Commute()
-rat:EnableATC(true)
 rat:SetROE("return")
+rat:ATC_Messages(false)
 rat:SetSpawnInterval(.5)
 rat:Spawn(amt)
 end
 
-spawnRAT("RAT_C17A", "Batumi", 200, 100, 2)
-spawnRAT("RAT_P51","Batumi",30, 75, 4)
-spawnRAT("RAT_Blackhawk","Batumi", 007, 50, 4)
+function spawnRATWithDest(groupName, depart, arrive, FL, dist, amt)
+local rat=RAT:New(groupName)
+rat:SetDeparture(depart)
+rat:SetDestination(arrive)
+rat:SetFLcruise(FL)
+rat:SetMaxDistance(dist)
+rat:SetTakeoff("runway")
+rat:Commute()
+rat:SetROE("return")
+rat:ATC_Messages(false)
+rat:SetSpawnInterval(.5)
+rat:Spawn(amt)
+end
+
+spawnRATWithDest("RAT_C17A", {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, {"Kobuleti", "Kutaisi", "Senaki-Kolkhi", "Batumi"}, 200, 100, 2)
+spawnRATWithDest("RAT_Blackhawk","Batumi", {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, 030, 50, 2)
+spawnRATWithDest("RAT_CE", {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, 050, 50, 3)
+spawnRATWithDest("RAT_CE2", {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, 030, 50, 4)
+spawnRATWithDest("RAT_YAK", {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, {"Kobuleti", "Kutaisi", "Senaki-Kolkhi"}, 040, 50, 4)
