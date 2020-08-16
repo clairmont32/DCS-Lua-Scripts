d0241b29ded7b0fa81c5a321a9b1cde5ec267ebd
Matthew Clairmont <matthew.clairmont1@gmail.com>
2020-08-15 21:48:16 -0400

    finish radio menus. start loading SpawnCap.lua

--- Dev/CapRadioMenus.lua
+++ Dev/CapRadioMenus.lua
@@ -8,33 +8,31 @@ Menu = MENU_MISSION:New("CAP Management")
 local function SpawnBVR(skill)
   local log = "Attempting to spawning BVR "..skill
   env.info(log)
-  if skill == "high" then
-    CapFlag = USERFLAG:New(90)
-    CapFlag:Set(2,0)
-  end
+  assert(loadfile("C:\\Users\\matth\\Saved Games\\DCS.openbeta\\Missions\\Scripts\\My Missions\\DCS-Lua-Scripts\\Dev\\SpawnCap.lua"))("bvr", skill)
 end
 
 -- spawn WVR CAP based on skill
 local function SpawnWVR(skill)
   local log = "Attempting to spawning WVR "..skill
-  env.info(log)
-  if skill == "high" then
-    CapFlag = USERFLAG:New(90)
-    CapFlag:Set(1,0)
-  end
+  env.info(log)  
+  assert(loadfile("C:\\Users\\matth\\Saved Games\\DCS.openbeta\\Missions\\Scripts\\My Missions\\DCS-Lua-Scripts\\Dev\\SpawnCap.lua"))("wvr", skill)
 end
 
 
 -- type management
+env.info("Creating type menus", false)
 local MenuBVR = MENU_MISSION:New("BVR Mgt", Menu)
 local MenuWVR = MENU_MISSION:New("WVR Mgt", Menu)
 
 -- BVR Menus
+env.info("Creating BVR Skill menus")
 local BVRHigh = MENU_MISSION_COMMAND:New("High", MenuBVR,SpawnBVR, "high")
 local BVRGood = MENU_MISSION_COMMAND:New("Good", MenuBVR, SpawnBVR, "good")
-local BVRAvg = MENU_MISSION_COMMAND:New("Avg", MenuBVR, SpawnBVR, "Avg")
+local BVRAvg = MENU_MISSION_COMMAND:New("Avg", MenuBVR, SpawnBVR, "avg")
 
 -- WVR Menus
+env.info("Creating WVR Skill menus")
 local WVRHigh = MENU_MISSION_COMMAND:New("WVR High", MenuWVR, SpawnWVR, "high")
 local WVRGood = MENU_MISSION_COMMAND:New("WVR Good", MenuWVR, SpawnWVR, "good")
 local WVRAvg = MENU_MISSION_COMMAND:New("WVR Avg", MenuWVR, SpawnWVR, "avg")
+local WVRGuns = MENU_MISSION_COMMAND:New("WVR Guns Only!", MenuWVR, SpawnWVR, "guns")
